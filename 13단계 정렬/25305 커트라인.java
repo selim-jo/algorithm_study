a060d15c7214fd2d6ac94e45e637a45f6246f09d
selim-jo <skyblue.777@daum.net>
2023-12-18 22:02:53 +0900

    231218 13단계 정렬(3)

---
+++ 13단계 정렬/25305 커트라인.java
@@ -0,0 +1,28 @@
+// 입력값: 첫째줄에는 응시자의 수 N과 상을 받는 사람의 수 k가 공백을 사이에 두고 주어짐
+//       둘쨰줄에는 각 학생의 점수 x가 공백을 사이에 두고 주어짐
+// 출력값: 상을 받는 커트라인 출력
+
+import java.util.*;
+
+public class Main {
+   public static void main(String args[]) {
+
+       Scanner sc = new Scanner(System.in);
+
+       // 입력값
+       int n = sc.nextInt();
+       int k = sc.nextInt();
+       Integer[] arr = new Integer[n];
+       
+       for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+       }
+
+       // 배열 오름차순으로 정렬
+       Arrays.sort(arr, Collections.reverseOrder());
+       
+       // 출력값
+       System.out.println(arr[k-1]);
+
+   }
+} 
