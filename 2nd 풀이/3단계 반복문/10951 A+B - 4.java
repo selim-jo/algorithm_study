98bdad5e9bd56f331bc7e556cd237c5b20da1add
selim-jo <skyblue.777@daum.net>
2024-02-15 22:52:24 +0900

    240215 2nd algorithm 풀이 3단계 반복문 2

---
+++ 2nd 풀이/3단계 반복문/10951 A+B - 4.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+   public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++) {
+            String s = br.readLine();
+            int A = Integer.paseInt(s.split(" ")[0]);
+            int B = Integer.parseInt(s.split(" ")[1]);
+            
+            bw.write((A+B) + "\n");
+        }
+        bw.flush();
+        bw.close();
+   }
+}
