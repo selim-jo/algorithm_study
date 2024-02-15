b04fe2b2300f85909e785897238d5a427d84b01c
selim-jo <skyblue.777@daum.net>
2024-02-16 08:19:50 +0900

    240216 2nd algorithm 풀이 4단계 1차원 배열(1-5)

---
+++ 2nd 풀이/4단계 1차원 배열/10807 개수 세기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+   public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       int N = sc.nextInt();
+       int[] arr = new int[N];
+       int v = sc.nextInt();
+       int count = 0;
+
+       for(int i=0; i<N; i++) {
+            arr[i] = sc.nextInt();
+       }
+
+       for(int j=0; j<arr.length; j++) {
+            if(v == arr[j]) {
+                count++
+            }
+       }
+       System.out.println(count);
+   }
+}
---
+++ 2nd 풀이/4단계 1차원 배열/2562 최댓값.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+   public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       int[] arr = new int[9];
+       int max = arr[0];
+       int index = 0;
+
+       for(int i=0; i<9; i++) {
+            arr[i] = sc.nextInt();
+       }
+
+       for(int j=0; j<arr.length; j++) {
+            if(arr[j] > max) {
+                max = arr[j];
+                index = j+1;
+            }
+       }
+       System.out.println(max);
+       System.out.println(index);
+   }
+}
