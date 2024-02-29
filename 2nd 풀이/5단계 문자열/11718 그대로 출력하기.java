1c8d64e159bc93d198ab63be7e5af73f3d0002b4
selim-jo <skyblue.777@daum.net>
2024-02-29 20:10:04 +0900

    240229 2nd algorithm 풀이 5단계 문자열(10-11)

---
+++ 2nd 풀이/5단계 문자열/11718 그대로 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+   public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+
+      // 입력값 있는지 여부 확인
+       while(sc.hasNext()) {
+            System.out.println(sc.nextLine());
+       }
+   }
+}
