1fdb6c37fd8e10e9f329e93c0c9b36c26aaa1689
selim-jo <skyblue.777@daum.net>
2024-03-04 12:01:24 +0900

    240304 2nd algorithm 풀이 6단계 심화1(1-2)

---
+++ 2nd 풀이/6단계 심화1/25083 새싹.java
@@ -0,0 +1,11 @@
+public class Main {
+   public static void main(String args[]) {
+        // 이스케이프 문자 이용 (\', \", \n, \\)
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+   }
+}
---
+++ 2nd 풀이/6단계 심화1/3003 킹, 퀸, 룩, 비숍, 나이트, 폰.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+   public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       
+       // 각 체스에 대한 값 설정
+       int king = 1;
+       int queen = 1;
+       int rook = 2;
+       int bishop = 2;
+       int knight = 2;
+       int pawn = 8;
+
+       king = king - sc.nextInt();
+       queen = queen - sc.nextInt();
+       rook = rook - sc.nextInt();
+       bishop = bishop - sc.nextInt();
+       knight = knight - sc.nextInt();
+       pawn = pawn - sc.nextInt();
+
+       System.out.print(king + " ");
+       System.out.print(queen + " ");
+       System.out.print(rook + " ");
+       System.out.print(bishop + " ");
+       System.out.print(knight + " ");
+       System.out.print(pawn + " ");
+   }
+}
