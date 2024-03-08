6663054f52bb4e941c1e48302a23a15ccb2c2245
selim-jo <skyblue.777@daum.net>
2024-03-08 21:06:56 +0900

    240308 2nd algorithm 풀이 7단계 2차원 배열(1-2)

---
+++ 2nd 풀이/7단계 2차원 배열/2566 최댓값.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+   public static void main(String args[]) {
+
+       Scanner sc = new Scanner(System.in);
+
+       // 입력
+       int[][] arr = new int[9][9];
+       int max = 0;
+       int row = 0;
+       int column = 0;
+        
+       // 최댓값 출력후 최댓값에 위치한 행 번호와 열 번호 출력
+       for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                arr[i][j] = sc.nextInt();
+                if(max < arr[i][j]) {
+                    max = arr[i][j];
+                    row = i;
+                    column = j;
+                }
+            }
+       }
+       System.out.println(max);
+       System.out.println((row + 1) + " " + (column + 1));
+   }
+}
---
+++ 2nd 풀이/7단계 2차원 배열/2738 행렬 덧셈.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+   public static void main(String args[]) {
+
+       Scanner sc = new Scanner(System.in);
+
+       int N = sc.nextInt();
+       int M = sc.nextInt();
+
+       int[][] arrA = new int[N][M];
+       int[][] arrB = new int[N][M];
+
+       // arrA 배열 입력값
+       for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                arrA[i][j] = sc.nextInt();
+            }
+       }
+
+       // arrB 배열 입력값
+       for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                arrB[i][j] = sc.nextInt();
+            }
+       }
+
+       // arrA + arrB 배열의 합
+       for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                System.out.print(arrA[i][j] + arrB[i][j] + " ");
+            
+                if(j == M-1) {
+                    System.out.println();
+                }
+            }
+       }
+   }
+}
