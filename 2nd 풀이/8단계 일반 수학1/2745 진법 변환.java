7f6f3b9b6a7a19912244ed9c8c42caaf9b467d29
selim-jo <skyblue.777@daum.net>
2024-03-13 21:34:25 +0900

    240313 2nd algorithm 풀이 8단계 일반 수학1(2)

---
+++ 2nd 풀이/8단계 일반 수학1/2745 진법 변환.java
@@ -0,0 +1,31 @@
+// 진법에 대한 이해 필요
+
+import java.util.*;
+
+public class Main {
+   public static void main(String args[]) {
+
+       Scanner sc = new Scanner(System.in);
+
+       String N = sc.next();
+       int B = sc.nextInt();
+       
+       int tmp = 1;
+       int ans = 0;
+
+       for(int i=N.length()-1; i>=0; i--) {
+            char c = N.charAt(i);
+
+            // 'A'-'Z' (65-90), 'a'-'z' (97-122)
+            // 'A'-'Z'는 숫자로 바꿔줌
+            if('A'<=c && c<='Z') {
+                ans += (c-'A'+10) * tmp;
+            } else {
+                ans += (c-'0') * tmp;
+            }
+            tmp *= B;
+       }
+
+       System.out.println(ans);
+   }
+}
