476171b1161b4738583985748f99ee73b537ea70
selim-jo <skyblue.777@daum.net>
2024-03-14 11:59:33 +0900

    240314 2nd algorithm 풀이 8단계 일반 수학1(3)

---
+++ 2nd 풀이/8단계 일반 수학1/2720 세탁소 사장 동혁.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+   public static void main(String args[]) {
+
+       Scanner sc = new Scanner(System.in);
+
+       // 입력값 (테스트 케이스의 개수)
+       int T = sc.nextInt();
+
+       // 출력값 (쿼터($0.25=25센트) 개수, 다임($0.10=10센트) 개수, 니켈($0.05=5센트) 개수, 페니($0.01=1센트) 개수)
+       for(int i=0; i<T; i++) {
+        
+            // 입력값 (거스름 돈 C(C의 단위는 센트, $1=100센트))
+            int C = sc.nextInt();
+
+            int quarter = C / 25;
+            C %= 25;
+
+            int dime = C / 10;
+            C %= 10;
+
+            int nickel = C / 5;
+            C %= 5;
+
+            int penny = C;
+
+            System.out.println(quarter + " " + dime + " " + nickel + " " + penny);
+       }
+   }
+}
